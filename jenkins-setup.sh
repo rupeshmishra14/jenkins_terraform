6006b9dafaf8b9e588dd9dcd1570f91da8c59e4a
rupeshmishra14 <75672984+rupeshmishra14@users.noreply.github.com>
2022-11-16 20:08:34 +0530

    Add files via upload

---
+++ jenkins-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash	
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo yum install java-1.8.0-openjdk.x86_64 -y
+sudo yum install git -y
+sudo yum install maven -y
